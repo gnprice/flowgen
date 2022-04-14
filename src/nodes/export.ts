5e6f3403a5f2dab6e1ea450989c3a94d513c9bc0
Greg Price <greg@zulip.com>
2022-04-14 14:19:10 -0700

    types: Use real types on two remaining Node subclasses

--- src/nodes/export.ts
+++ src/nodes/export.ts
@@ -1,10 +1,10 @@
-import type { RawNode } from "./node";
+import ts from "typescript";
 import Node from "./node";
 
 import * as printers from "../printers";
 
-export default class Export extends Node {
-  constructor(node: RawNode) {
+export default class Export extends Node<ts.ExportAssignment> {
+  constructor(node: ts.ExportAssignment) {
     super(node);
   }
 
