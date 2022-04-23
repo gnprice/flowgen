b9693be038f32d30f2c5c885a4cfe1f385a608ba
Greg Price <greg@zulip.com>
2022-04-23 16:20:58 -0700

    WIP add test for typeof

---
+++ src/__tests__/typeof.spec.ts
@@ -0,0 +1,14 @@
+import { compiler, beautify } from "..";
+import "../test-matchers";
+
+it("should handle `typeof`", () => {
+  const ts = `
+export const x = { o: { f(string): number } };
+export type Tx = typeof x;
+export type Txo = typeof x.o;
+export type Txof = typeof x.o.f;
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+  expect(result).toBeValidFlowTypeDeclarations();
+});
