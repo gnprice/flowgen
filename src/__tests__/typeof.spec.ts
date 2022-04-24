4d83b12c8ac809c10675a58a93a36000a74f38c2
Greg Price <greg@zulip.com>
2022-04-24 14:28:40 -0700

    wip typeof tests: Compare TS namespaces with new and old ("module") syntax
    Hmmm, I expected this to fail given the `NodeFlags.Namespace`
    reference in `isModule` called from `getTypeofFQN`.

--- src/__tests__/typeof.spec.ts
+++ src/__tests__/typeof.spec.ts
@@ -12,6 +12,19 @@ export namespace n { export const x: { y: number } }
 export type N = typeof n;
 export type Nx = typeof n.x;
 export type Nxy = typeof n.x.y;
+
+export namespace n { export namespace nn { export const x: number } }
+export namespace n { export module mm { export const x: number } }
+
+// These two should behave exactly the same -- a TS "module" declaration is
+// just the old syntax for a TS "namespace", which were originally called
+// "internal modules".
+export type Nnn = typeof n.nn;
+export type Nmm = typeof n.mm;
+
+// Similarly these two.
+export type Nnnx = typeof n.nn.x;
+export type Nmmx = typeof n.mm.x;
 `;
   const result = compiler.compileDefinitionString(ts);
   expect(beautify(result)).toMatchSnapshot();
