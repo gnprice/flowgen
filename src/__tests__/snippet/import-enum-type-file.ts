aafc65f110ef01b05d33e8416ca2bd6d6bc07109
Greg Price <greg@zulip.com>
2022-04-24 12:51:21 -0700

    wip enum tests: add type-import counterparts to new import tests
    Turning up one more bug: `import typeof` on something that's already a
    type, a type alias of an enum.

--- src/__tests__/snippet/import-enum-type-file.ts
+++ src/__tests__/snippet/import-enum-type-file.ts
@@ -1,4 +1,25 @@
-import type { Label } from "./export-enum-file";
-export function foo(label: Label): void {
+import type {
+  Label as ImportedLabel,
+  LabelA,
+  LLabel, // TODO BROKEN: gets `import typeof`
+} from "./export-enum-file";
+import type {
+  Label as ImportedLabelR,
+  LabelA as LabelAR,
+  LLabel as LLabelR, // TODO BROKEN: gets `import typeof`
+} from "./reexport-enum-file";
+
+// Plus the same brokenness as in import-enum-file.ts from e.g. renaming on import.
+
+export function foo(label: ImportedLabel): void {
   console.log(label);
 }
+
+export type WM = ImportedLabel.A;
+export type X = LabelA;
+export type Y = LLabel;
+
+export type WR = ImportedLabelR;
+export type WRM = ImportedLabelR.A;
+export type XR = LabelAR;
+export type YR = LLabelR;
