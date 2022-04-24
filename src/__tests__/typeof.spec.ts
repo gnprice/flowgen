9f3c4f17698042e76bff89aa874afb9866ff35e1
Greg Price <greg@zulip.com>
2022-04-24 13:22:15 -0700

    wip typeof tests: through a namespace
    The current output works, but seems unnecessarily complicated and
    unnecessarily divergent from the TS: it says e.g. `typeof n$x.y`.
    Why not simply `typeof n.x.y`?  After all, `n` is a value.
    
    More generally: when we need to refer to a value (which I think
    corresponds exactly to when we're under a `typeof`, either the
    operator or the import modifier), we should be able to refer to
    namespaces directly.  TS compiles them to good old JS objects; so the
    user's actual runtime code can refer to them via good old
    property-access syntax; so in turn our rendering of the TS type
    definition must allow that; so we might as well do the same thing
    ourselves.

--- src/__tests__/typeof.spec.ts
+++ src/__tests__/typeof.spec.ts
@@ -4,9 +4,14 @@ import "../test-matchers";
 it("should handle `typeof`", () => {
   const ts = `
 export const x: { o: { f(string): number } };
-// export type Tx = typeof x;
-// export type Txo = typeof x.o;
+export type Tx = typeof x;
+export type Txo = typeof x.o;
 export type Txof = typeof x.o.f;
+
+export namespace n { export const x: { y: number } }
+export type N = typeof n;
+export type Nx = typeof n.x;
+export type Nxy = typeof n.x.y;
 `;
   const result = compiler.compileDefinitionString(ts);
   expect(beautify(result)).toMatchSnapshot();
