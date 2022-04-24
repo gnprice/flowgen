4cccbfe34a964a50ca31fd3f7dbf33dd9c067290
Greg Price <greg@zulip.com>
2022-04-24 11:54:54 -0700

    wip enums: Add test for broken enum handling across imports

--- src/__tests__/snippet/export-enum-file.ts
+++ src/__tests__/snippet/export-enum-file.ts
@@ -2,3 +2,6 @@ export enum Label {
   A = "A",
   B = "B",
 }
+
+export type LabelA = Label.A;
+export type LLabel = Label;
--- src/__tests__/snippet/import-enum-file.ts
+++ src/__tests__/snippet/import-enum-file.ts
@@ -1,4 +1,8 @@
-import { Label } from "./export-enum-file";
-export function foo(label: Label): void {
+import { Label as ImportedLabel, LabelA, LLabel } from "./export-enum-file";
+
+export function foo(label: ImportedLabel): void {
   console.log(label);
 }
+
+export type X = LabelA;
+export type Y = LLabel;
