4412bf96e80572bd63f265558ac8767cca27a111
Greg Price <greg@zulip.com>
2022-04-24 12:10:03 -0700

    wip enums: more test cases: wrong for member of import-renamed enum

--- src/__tests__/snippet/import-enum-file.ts
+++ src/__tests__/snippet/import-enum-file.ts
@@ -9,9 +9,11 @@ export function foo(label: ImportedLabel): void {
   console.log(label);
 }
 
+export type WM = ImportedLabel.A;
 export type X = LabelA;
 export type Y = LLabel;
 
 export type WR = ImportedLabelR;
+export type WRM = ImportedLabelR.A;
 export type XR = LabelAR;
 export type YR = LLabelR;
